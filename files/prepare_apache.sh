68dfc5834af6595f6b417704a4253088c66a0a9e
Mike Petersen <mike@odania-it.de>
2016-10-10 00:27:37 +0200

    Enabled rewrite and updated logging

--- files/prepare_apache.sh
+++ files/prepare_apache.sh
@@ -3,8 +3,12 @@
 chown -R apache:apache /srv
 mkdir -p /run/apache2 && chown -R apache:apache /run/apache2
 sed -i 's#^DocumentRoot ".*#DocumentRoot "/srv/wordpress"#g' /etc/apache2/httpd.conf
-sed -i 's#AllowOverride none#AllowOverride All#g' /etc/apache2/httpd.conf
+sed -i 's#AllowOverride [nN]one#AllowOverride All#g' /etc/apache2/httpd.conf
 sed -i 's#^<Directory ".*>#<Directory "/srv/wordpress">#g' /etc/apache2/httpd.conf
+sed -i 's/#LoadModule rewrite_module/LoadModule rewrite_module/' /etc/apache2/httpd.conf
+sed -i 's#ErrorLog logs/error.log#/dev/stderr#g' /etc/apache2/httpd.conf
+
+echo "CustomLog /dev/stdout combined" >> /etc/apache2/httpd.conf
 
 # logs should go to stdout / stderr
 ln -sfT /dev/stderr "/var/log/apache2/error.log"
