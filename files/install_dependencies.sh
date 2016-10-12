5a3bf155f7cab80eb3114fcf20bbca4a5c7ffc03
Mike Petersen <mike@odania-it.de>
2016-10-12 22:36:42 +0200

    Added php ctype

--- files/install_dependencies.sh
+++ files/install_dependencies.sh
@@ -3,7 +3,7 @@
 apk update
 apk upgrade
 apk --update add curl wget bash htop vim msmtp
-apk --update add php5-apache2 php5-imagick php5-zlib php5-mysqli php5-gd php5-cli php5-json php5-openssl php5-xml php5-xmlreader php5-iconv
+apk --update add php5-apache2 php5-imagick php5-zlib php5-mysqli php5-gd php5-cli php5-json php5-openssl php5-xml php5-xmlreader php5-iconv php5-ctype
 
 # Clean APK cache
 rm -rf /var/cache/apk/*
